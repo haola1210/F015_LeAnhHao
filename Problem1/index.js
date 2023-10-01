dfc1a0e8c941903f45d0e5ed9b43395e8fc86df1
haola1210 <haola1210@gmail.com>
2023-10-01 14:54:26 +0700

    Problem1: implement as requirement

---
+++ Problem1/index.js
@@ -0,0 +1,78 @@
+const ErrorMsg = {
+  CALC_LIMIT: 'The result is greater than Number.MAX_SAFE_INTEGER',
+  STACK_LIMIT: 'Maximum callstack size exceeded'
+}
+
+const runWithTimeTracking = (cb) => {
+  const start = performance.now();
+  const result = cb()
+  const end = performance.now();
+  const executionTime = end - start;
+  const title = cb.toString().replace('() => ', '')
+  if (result !== undefined) {
+    console.table({
+      [title]: {
+        result,
+        execution_time: `${executionTime.toFixed(6)} ms`
+      }
+    })
+  } else {
+    console.log(`The execution of ${title} failed`)
+  }
+}
+
+/**
+ * Time complexity: O(n)
+ */
+const sum_to_n_a = n => {
+  try {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  } catch (error) {
+    console.error(ErrorMsg.CALC_LIMIT)
+  }
+
+}
+
+/**
+ * Using the Gauss Summation
+ */
+const sum_to_n_b = n => {
+  try {
+    return n * (n + 1) / 2
+  } catch (error) {
+    console.error(ErrorMsg.CALC_LIMIT)
+  }
+}
+
+/**
+ * Another way is recursion. But not recommend this approach
+ */
+const sum_to_n_c = n => {
+  try {
+    if (n === 0) {
+      return 0;
+    }
+
+    const v = sum_to_n_c(n-1);
+    if (v === undefined) return v;
+    
+    return n + v;
+  } catch (error) {
+    console.error(ErrorMsg.STACK_LIMIT)
+  }
+}
+
+// run normally
+
+// console.log(sum_to_n_a(1000))
+// console.log(sum_to_n_b(1000))
+// console.log(sum_to_n_c(1000))
+
+// run with time tracking
+runWithTimeTracking(() => sum_to_n_a(1000))
+runWithTimeTracking(() => sum_to_n_b(1000))
+runWithTimeTracking(() => sum_to_n_c(1000))
